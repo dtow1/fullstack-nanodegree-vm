e00b97ae6eb654e614da3519e9c2736b4a6bcda5
Jason <jboy81@gmail.com>
2016-08-27 11:40:20 -0700

    some changes following course

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,11 +1,11 @@
 #
 # Database access functions for the web forum.
-# 
+#
 
 import time
-
+import psycopg2
+import bleach
 ## Database connection
-DB = []
 
 ## Get posts from database.
 def GetAllPosts():
@@ -16,8 +16,15 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
-    posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
+    cursor.execute("SELECT time,content FROM posts ORDER BY time DESC")
+    posts = ({'content':str(row[1]),'time':str(row[0])}
+              for row in cursor.fetchall())
+    bleach.clean(posts)
+    DB.close()
+    # posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
+    # posts.sort(key=lambda row: row['time'], reverse=True)
     return posts
 
 ## Add a post to the database.
@@ -27,5 +34,10 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    DB = psycopg2.connect("dbname=forum")
+    cursor = DB.cursor()
+    cursor.execute("INSERT INTO posts (content) VALUES (%s)", bleach.clean((content,)))
+    DB.commit()
+    DB.close()
+    # t = time.strftime('%c', time.localtime())
+    # DB.append((t, content))
